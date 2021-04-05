f796d5dcc491a7a1831e35f57bb8affc3e31f755
Jesse <jessemckinney513@gmail.com>
2021-04-05 12:49:16 -0400

    yay

--- App.js
+++ App.js
@@ -1,38 +1,89 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default class App extends Component {
   state = {
-    location: null,
+    // location: null,
+    // location: [],
+    myLat: "",
+    myLon: "",
+    city: "",
+    state: "",
+    zip: "",
+    weatherDescription: "",
+    temp: "",
+    humidity: "",
+    windSpeed: "",
+    placesToGolf: [],
   };
 
-  findCoordinates = () => {
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const location = JSON.stringify(position);
-
-        this.setState({ location });
+      (pos) => {
+        let coordinates = pos.coords;
+        this.setState({ myLat: coordinates.latitude });
+        this.setState({ myLon: coordinates.longitude });
+        // const location = JSON.stringify(pos);
+        // this.setState({ location });
       },
-      (error) => Alert.alert(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      (error) => Alert.alert(error.message, error.code),
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
     );
+    console.log(this.state.myLat);
+    console.log(this.state.myLon);
+  }
+
+  findCoordinates = () => {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     const location = JSON.stringify(position);
+    //     this.setState({ location });
+    //   },
+    //   (error) => Alert.alert(error.message),
+    //   { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    // );
+    // console.log(this.state.location);
+  };
+
+  getForeCast = (e) => {
+    e.preventDefault();
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.findCoordinates}>
-          <Text style={styles.welcome}>Find My Coords?</Text>
-          <Text>Location: {this.state.location}</Text>
-        </TouchableOpacity>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={this.findCoordinates}>
+            <Text style={styles.welcome}>Find My Coords?</Text>
+            {/* <Text style={styles.welcome}>Location: {this.state.location}</Text> */}
+            <Text style={styles.welcome}>Latitude: {this.state.myLat}</Text>
+            <Text style={styles.welcome}>Longitude: {this.state.myLon}</Text>
+          </TouchableOpacity>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
