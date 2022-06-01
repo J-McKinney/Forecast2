cd37f37e9dc28a9365525b9f95582e866fc3257e
Jesse <jessemckinney513@gmail.com>
2022-06-01 11:48:04 -0400

    yay

--- App.js
+++ App.js
@@ -281,6 +281,7 @@ export default class App extends Component {
                           type="star"
                           ratingCount={5}
                           showReadOnlyText="true"
+                          readonly="true"
                           startingValue={filteredPlaces.rating}
                         />
                         <View style={{ height: "20px" }} />
